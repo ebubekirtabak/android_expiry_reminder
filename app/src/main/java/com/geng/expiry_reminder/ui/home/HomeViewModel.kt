a2554497e3ae0d5b142a0b5267e1b509e2b6976d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-09-11 15:32:10 +0300

    update category list data

--- app/src/main/java/com/geng/expiry_reminder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/geng/expiry_reminder/ui/home/HomeViewModel.kt
@@ -2,15 +2,19 @@ package com.geng.expiry_reminder.ui.home
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.geng.expiry_reminder.R
 import com.geng.expiry_reminder.adapters.CategoryViewAdapter
 import com.geng.expiry_reminder.models.category.Category
 
 class HomeViewModel : ViewModel() {
     private val _categoryViewAdapter = MutableLiveData<CategoryViewAdapter>().apply {
         val categoryList = arrayListOf<Category>(
-            Category("Fridge", 0, 1),
-            Category("Documents", 0, 2),
-            Category("Payment", 0, 1)
+            Category("Fridge", R.drawable.ic_baseline_food_bank_24, 1, R.color.purple_500),
+            Category("Freezer", R.drawable.ic_baseline_kitchen_24, 2, R.color.blue),
+            Category("Pantry", R.drawable.ic_cupboard_black_24, 3, R.color.gray),
+            Category("Documents", R.drawable.ic_baseline_payments_24, 4, R.color.black),
+            Category("Payments", R.drawable.ic_baseline_shopping_cart_24, 5, R.color.purple_500) ,
+            Category("Medicine", R.drawable.ic_baseline_medical_services_24, 6, R.color.red)
         )
         value = CategoryViewAdapter(categoryList)
     }
