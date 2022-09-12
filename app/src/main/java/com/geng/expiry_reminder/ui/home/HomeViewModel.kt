5fd83fd7c6c3e2056b1ddf7036f9748e68b374a2
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-09-12 22:58:06 +0300

    update document item color

--- app/src/main/java/com/geng/expiry_reminder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/geng/expiry_reminder/ui/home/HomeViewModel.kt
@@ -12,7 +12,7 @@ class HomeViewModel : ViewModel() {
             Category(0,"Fridge", R.drawable.ic_baseline_food_bank_24, 1, R.color.purple_500),
             Category(0,"Freezer", R.drawable.ic_baseline_kitchen_24, 2, R.color.blue),
             Category(0,"Pantry", R.drawable.ic_cupboard_black_24, 3, R.color.gray),
-            Category(0,"Documents", R.drawable.ic_baseline_payments_24, 4, R.color.black),
+            Category(0,"Documents", R.drawable.ic_baseline_insert_drive_file_24, 4, R.color.deep_jungle_green),
             Category(0,"Payments", R.drawable.ic_baseline_shopping_cart_24, 5, R.color.purple_500) ,
             Category(0,"Medicine", R.drawable.ic_baseline_medical_services_24, 6, R.color.red)
         )
