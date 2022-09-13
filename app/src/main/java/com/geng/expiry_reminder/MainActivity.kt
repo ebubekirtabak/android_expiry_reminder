3fdd605cf615b7aa9bcaa10ab2dfc514e7e06f2e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-09-13 21:11:17 +0300

    init categories in first time

--- app/src/main/java/com/geng/expiry_reminder/MainActivity.kt
+++ app/src/main/java/com/geng/expiry_reminder/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.geng.expiry_reminder
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -8,21 +10,26 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
+import com.geng.expiry_reminder.database.AppDatabase
+
 import com.geng.expiry_reminder.databinding.ActivityMainBinding
+import com.geng.expiry_reminder.models.category.Category
+import com.geng.expiry_reminder.models.category.CategoryDao
 import com.geng.expiry_reminder.preferences.SettingsPreferences
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         var isFirstRun: Boolean = false
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -55,6 +62,19 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        val categoryList = arrayListOf<Category>(
+            Category(0,"Fridge", R.drawable.ic_baseline_food_bank_24, 1, R.color.purple_500),
+            Category(0,"Freezer", R.drawable.ic_baseline_kitchen_24, 2, R.color.blue),
+            Category(0,"Pantry", R.drawable.ic_cupboard_black_24, 3, R.color.gray),
+            Category(0,"Documents", R.drawable.ic_baseline_insert_drive_file_24, 4, R.color.deep_jungle_green),
+            Category(0,"Payments", R.drawable.ic_baseline_shopping_cart_24, 5, R.color.purple_500) ,
+            Category(0,"Medicine", R.drawable.ic_baseline_medical_services_24, 6, R.color.red)
+        )
+        val applicationScope = CoroutineScope(SupervisorJob())
+        val database by lazy { AppDatabase.getDatabase(this, applicationScope) }
+        database.CategoryDao().insertAll(categoryList)
+        val category1 = database.CategoryDao().findByName("Doc")
+        Log.d("Category", category1.name);
         settingsPreferences.setIsFirstRun(false)
     }
 }
