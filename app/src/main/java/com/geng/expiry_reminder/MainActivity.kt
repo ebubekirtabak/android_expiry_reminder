34f59d0166032fc5e0486e067f9dd57621a0dcf6
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-09-15 18:59:04 +0300

    migrate first insert to database builder

--- app/src/main/java/com/geng/expiry_reminder/MainActivity.kt
+++ app/src/main/java/com/geng/expiry_reminder/MainActivity.kt
@@ -8,14 +8,10 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.geng.expiry_reminder.database.AppDatabase
 
 import com.geng.expiry_reminder.databinding.ActivityMainBinding
-import com.geng.expiry_reminder.models.category.Category
 import com.geng.expiry_reminder.preferences.SettingsPreferences
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -56,19 +52,6 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val categoryList = arrayListOf<Category>(
-            Category(0,"Fridge", R.drawable.ic_baseline_food_bank_24, 1, R.color.purple_500),
-            Category(0,"Freezer", R.drawable.ic_baseline_kitchen_24, 2, R.color.blue),
-            Category(0,"Pantry", R.drawable.ic_cupboard_black_24, 3, R.color.gray),
-            Category(0,"Documents", R.drawable.ic_baseline_insert_drive_file_24, 4, R.color.deep_jungle_green),
-            Category(0,"Payments", R.drawable.ic_baseline_shopping_cart_24, 5, R.color.purple_500) ,
-            Category(0,"Medicine", R.drawable.ic_baseline_medical_services_24, 6, R.color.red)
-        )
-        val applicationScope = CoroutineScope(SupervisorJob())
-        val database by lazy { AppDatabase.getDatabase(this, applicationScope) }
-        database.CategoryDao().insertAll(categoryList)
-        val category1 = database.CategoryDao().findByName("Doc")
-        Log.d("Category", category1.name);
         settingsPreferences.setIsFirstRun(false)
     }
 }
