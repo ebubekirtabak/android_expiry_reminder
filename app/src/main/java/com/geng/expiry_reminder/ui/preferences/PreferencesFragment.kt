acb5e476130245b4ff973a54997ea06dc6c79b2d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-10-02 21:23:41 +0300

    update settings preferences

--- app/src/main/java/com/geng/expiry_reminder/ui/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/geng/expiry_reminder/ui/preferences/PreferencesFragment.kt
@@ -1,12 +1,30 @@
 package com.geng.expiry_reminder.ui.preferences
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.geng.expiry_reminder.R
+import com.geng.expiry_reminder.utils.AppThemeUtils
 
-class PreferencesFragment: PreferenceFragmentCompat() {
+class PreferencesFragment: PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.app_preferences, rootKey)
+
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        preferenceManager?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        when (p1) {
+            "theme_style" -> {
+                val theme = p0?.getString("theme_style", "system").toString()
+                AppThemeUtils().updateAppTheme(theme)
+            }
+        }
     }
 }
